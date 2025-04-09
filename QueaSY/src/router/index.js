10d6bbfd7eb6ae26b1f78ea003f0d6647f5bac86
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-04-09 12:12:34 -0400

    Nausicaa

--- QueaSY/src/router/index.js
+++ QueaSY/src/router/index.js
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import { callWithAsyncErrorHandling } from "vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/home",
+      name: "home",
+      component: HomePage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/tank",
+      name: "Fish Tank",
+      component: () => import("../views/FishTank.vue"),
     },
+    {},
   ],
-})
+});
 
-export default router
+export default router;
