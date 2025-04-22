16e104427a3ce03f46b8e31874ac6ce156b47468
AlexanderDong09 <alexanderdong09@gmail.com>
2025-04-22 12:32:49 -0400

    ghuhhuh?

--- QueaSY/src/stores/auth.js
+++ QueaSY/src/stores/auth.js
@@ -6,18 +6,32 @@ export const useAuthStore = defineStore("auth", {
     user: null,
   }),
   actions: {
-    login(username, password) {
-      // Placeholder credentials
-      const dummyUser = { username: "bob", password: "fish" };
-
-      if (username === dummyUser.username && password === dummyUser.password) {
-        this.user = { username: dummyUser.username };
-        return true;
-      }
-      return false;
+    async signUp(email, password) {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+      this.error = error;
+      if (!error) this.user = data.user;
     },
-    logout() {
+
+    async signIn(email, password) {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      this.error = error;
+      if (!error) this.user = data.user;
+    },
+
+    async signOut() {
+      await supabase.auth.signOut();
       this.user = null;
+    },
+
+    async fetchUser() {
+      const { data } = await supabase.auth.getUser();
+      this.user = data.user;
     },
   },
   getters: {
