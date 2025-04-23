a410e677d61b4fd4d60104b35431c7bfc50cdecb
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-04-23 12:07:50 -0400

    Oarm ruined my code

---
+++ QueaSY/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useAuthStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
+
+const store = useAuthStore();
+
+const { user } = storeToRefs(store);
+</script>
+
+<template>
+  <main>
+    <p v-if="!user">
+      nah🥀, you 🥀 ,not 🥀, logged 🥀, in 🥀,twin 🥀, awh 🥀, hell 🥀,nah
+      🥀,twin 🥀, you 🥀 ,log🥀, tf 🥀, in 🥀, twin 🥀, who 🥀, is 🥀, this🥀,
+      twin 🥀, on 🥀,foenem 🥀 ,grave 🥀,bruh 🥀 awh 🥀, hell 🥀,nah 🥀,twin 🥀,
+      you 🥀 ,not 🥀, logged 🥀, in 🥀,twin 🥀, you 🥀, need 🥀, to 🥀, log🥀,
+      tf 🥀, in 🥀, twin 🥀, who 🥀, is 🥀, this🥀, twin 🥀, on 🥀,foenem 🥀
+      ,grave 🥀,bruh 🥀
+    </p>
+    <p v-if="user">
+      aw 🌹 , hell🌹 , yeah🌹, twin🌹, you🌹, logged 🌹, in 🌹,twin 🌹, this🌹,
+      is🌹, you🌹, twin🌹 IS🌹, you🌹, twin🌹
+    </p>
+  </main>
+</template>
