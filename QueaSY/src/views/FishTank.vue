0919dfce08b7106edd3a19ea2c76ef8069047e93
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-04-28 03:58:21 -0400

    Chat gpted tailwind again. Bedtime now

--- QueaSY/src/views/FishTank.vue
+++ QueaSY/src/views/FishTank.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="w-full h-full">
+  <div class="w-150 h-120>">
+    <div class="w-7/8 h-3/4 bg-blue-600 mt-6 grid grid-cols-3 gap-4">
+      <div
+        v-for="(item, index) in rolledItems"
+        :key="index"
+        class="bg-white p-4 rounded-lg shadow-lg"
+      >
+        <img
+          :src="item.image"
+          :alt="item.name"
+          class="w-24 h-24 object-contain mx-auto"
+        />
+        <p class="text-center mt-2">{{ item.name }}</p>
+      </div>
+    </div>
     <div class="flex justify-around bg-blue-400 w-7/8">
       <div>
         <RouterLink to="/HL">Higher or Lower Game</RouterLink>
@@ -42,21 +56,38 @@ import { ref, reactive } from "vue";
 import { fishList } from "@/fishList.js";
 
 const result = ref(null);
+const rolledItems = reactive([]);
 
 function rollGacha(list) {
   const totalChance = list.reduce((sum, fish) => sum + fish.chance, 0);
   const random = Math.random() * totalChance;
   let accumulatedChance = 0;
+  let selectedItem = null;
   for (let fish of list) {
     accumulatedChance += fish.chance;
     if (random < accumulatedChance) {
       result.value = fish;
+      selectedItem = fish;
       break;
     }
   }
+  if (selectedItem) {
+    rolledItems.push(selectedItem);
+  }
+  gachaCost();
 }
 
 function closeModal() {
   result.value = null;
+}
+
+function gachaCost() {
+  let moneyAmount = parseInt(localStorage.getItem("coins"));
+  if (moneyAmount >= 10) {
+    let newAmount = moneyAmount - 10;
+    localStorage.setItem("coins", newAmount);
+  } else {
+    alert("Not enough coins!");
+  }
 }
 </script>
