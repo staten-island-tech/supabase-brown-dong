8946b5b46470f5cc002f5828acaf3d9095b6b678
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-05-13 12:30:29 -0400

    Figure this out later

--- QueaSY/src/views/FishTank.vue
+++ QueaSY/src/views/FishTank.vue
@@ -47,43 +47,10 @@
           </div>
         </div>
         <div>
-          <h1>REMOVE BUTTON HERE</h1>
+          <button @click="removeFish(rolledItems)">REMOVE BUTTON HERE</button>
         </div>
       </div>
     </div>
-    <!-- <div class="flex justify-around bg-blue-400 w-7/8">
-      <div>
-        <RouterLink to="/HL">Higher or Lower Game</RouterLink>
-      </div>
-      <div>
-        <h1>Welcome to the Gacha Game!</h1>
-        <button @click="rollGacha(fishList)">Roll</button>
-        <div
-          v-if="result"
-          class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50"
-        >
-          <div class="bg-blue-400 p-6 rounded-lg shadow-xl w-80 text-center">
-            <h2 class="text-lg font-semibold mb-4">
-              You got: {{ result.name }}!
-            </h2>
-            <img
-              :src="result.image"
-              alt="Gacha Item"
-              class="w-48 h-48 object-contain mx-auto mb-4"
-            />
-            <button
-              @click="closeSquare"
-              class="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-400"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-      <div>
-        <button @click="removeFish()">REMOVE BUTTON HERE</button>
-      </div>
-    </div> -->
   </div>
 </template>
 
@@ -118,8 +85,8 @@ function rollGacha(list) {
     }
   }
 }
-function removeFish() {
-  selectedItem = [];
+function removeFish(rolledItems) {
+  rolledItems = [];
 }
 function closeSquare() {
   result.value = null;
