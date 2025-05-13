4fab9c42bee0c4b8df1735f8be2fd27eacbda9c5
alexander <alexanderdong09@gmail.com>
2025-05-13 11:44:53 -0400

    a lil bit of fixing

--- QueaSY/src/views/HomePage.vue
+++ QueaSY/src/views/HomePage.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <div class="w-150 h-120 bg-red-500">
-      <h1>Log in will be here</h1>
-    </div>
+  <div
+    class="bg-red-500 absolute w-[80rem] h-[50rem] flex flex-col top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 justify-center"
+  >
+    <h1>Sign Up with me Twan 🌹</h1>
+
+    <h2>Create an Account</h2>
+    <form @submit.prevent="handleSignup">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input
+        v-model="password"
+        type="password"
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+    <p v-if="auth.error" style="color: red">{{ auth.error.message }}</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useAuthStore } from "@/stores/auth.js";
+import { useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
 
-<style lang="scss" scoped></style>
+const email = ref("");
+const password = ref("");
+const auth = useAuthStore();
+const { user, error } = storeToRefs(auth);
+const router = useRouter();
+
+async function handleSignup() {
+  await auth.signUp(email.value, password.value);
+
+  if (auth.user) {
+    console.log("Signup success!", auth.user);
+  } else {
+    console.error("Signup failed:", auth.error);
+  }
+}
+</script>
