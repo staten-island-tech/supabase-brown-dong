27596ddab8b45a3290f3b4aa6a7865c5dcc2ebd1
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-05-27 12:31:32 -0400

    Mr Whalen would be a Darmanitan

--- QueaSY/src/views/StorePage.vue
+++ QueaSY/src/views/StorePage.vue
@@ -3,13 +3,21 @@
     <div>
       <canvas
         id="ideeznuts"
-        class="w-150 h-150 border-2 absolute translate-y-1/2 translate-x-1/2"
-        ><img src="/Poliwhirl.png" class="w-150 h-150" alt="The goat"
+        class="w-150 h-75 border-2 absolute translate-y-1/2 translate-x-1/2"
+        ><img src="/Poliwhirl.png" class="w-150 h-75" alt="The goat"
       /></canvas>
     </div>
   </div>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup>
+function unction() {
+  const canvas = document.getElementById("ideeznuts");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "rgb(200 0 0)";
+  ctx.fillRect(10, 10, 50, 50);
+  ctx.fillStyle = "rgb(0 0 200 / 50%)";
+  ctx.fillRect(30, 30, 50, 50);
+}
+unction();
+</script>
