4013bba0a1116ecebb9a1ec6d1c67aba98f89c7f
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-05-27 12:10:04 -0400

    Magikarp
    Whalen is like Darmanitan

--- QueaSY/src/fishList.js
+++ QueaSY/src/fishList.js
@@ -1,6 +1,7 @@
 export const fishList = [
-  { name: "White Axolotl", chance: 50, image: "WhiteAxolotl.png" },
-  { name: "Blue Pikmin", chance: 30, image: "BluePikmin.png" },
-  { name: "Poliwhirl", chance: 12, image: "Poliwhirl.png" },
-  { name: "Spongebob Banana", chance: 8, image: "SpongebobBanana.png" },
+  { name: "White Axolotl", chance: 40, image: "WhiteAxolotl.png" },
+  { name: "Magikarp", chance: 25, image: "Magikarp.png" },
+  { name: "Blue Pikmin", chance: 20, image: "BluePikmin.png" },
+  { name: "Poliwhirl", chance: 10, image: "Poliwhirl.png" },
+  { name: "Spongebob Banana", chance: 5, image: "SpongebobBanana.png" },
 ];
