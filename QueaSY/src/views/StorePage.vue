a08b2f51c2cfea596968b79569c4ae1363798333
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-05-28 12:31:38 -0400

    I got svg noiw

--- QueaSY/src/views/StorePage.vue
+++ QueaSY/src/views/StorePage.vue
@@ -1,23 +1,22 @@
 <template>
   <div>
     <div>
-      <canvas
-        id="ideeznuts"
-        class="w-150 h-75 border-2 absolute translate-y-1/2 translate-x-1/2"
-        ><img src="/Poliwhirl.png" class="w-150 h-75" alt="The goat"
-      /></canvas>
+      <svg
+        version="1.1"
+        width="300"
+        height="200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="100%" height="100%" fill="red" />
+
+        <circle cx="150" cy="100" r="80" fill="green" />
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">
+          SVG
+        </text>
+      </svg>
     </div>
   </div>
 </template>
 
-<script setup>
-function unction() {
-  const canvas = document.getElementById("ideeznuts");
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "rgb(200 0 0)";
-  ctx.fillRect(10, 10, 50, 50);
-  ctx.fillStyle = "rgb(0 0 200 / 50%)";
-  ctx.fillRect(30, 30, 50, 50);
-}
-unction();
-</script>
+<script setup></script>
