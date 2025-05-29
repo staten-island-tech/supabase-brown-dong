4c48112bd75557ebfbc5b2ad7a07832341b5ba30
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-05-29 12:32:46 -0400

    Homemade graphics

--- QueaSY/src/views/StorePage.vue
+++ QueaSY/src/views/StorePage.vue
@@ -22,10 +22,23 @@ function draw() {
   requestAnimationFrame(draw);
 }
 function create() {
-  const canvas = document.getElementById("MarkFridlin");
-  const ctx = canvas.getContext("2d");
+  const canvas2 = document.getElementById("MarkFridlin");
+  const ctx2 = canvas2.getContext("2d");
+  ctx2.beginPath();
+  ctx2.moveTo(75, 475);
+  ctx2.lineTo(125, 475);
+  ctx2.lineTo(125, 465);
+  ctx2.lineTo(135, 465);
+  ctx2.lineTo(135, 485);
+  ctx2.lineTo(125, 485);
+  ctx2.lineTo(125, 500);
+  ctx2.lineTo(120, 500);
+  ctx2.lineTo(120, 485);
+  ctx2.lineTo(75, 500);
+  ctx2.fill();
 }
 onMounted(() => {
   draw();
+  create();
 });
 </script>
