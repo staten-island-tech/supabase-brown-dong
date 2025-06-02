e13b8c5cb406be1c02decef1152caa4ee406f2b8
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-06-02 12:29:33 -0400

    Incomprehensible

--- QueaSY/src/views/StorePage.vue
+++ QueaSY/src/views/StorePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div><canvas id="ideeznuts" width="500" height="500"></canvas></div>
-    <div><canvas id="MarkFridlin" width="500" height="500"></canvas></div>
+    <div class="box"></div>
   </div>
 </template>
 
@@ -21,26 +21,7 @@ function draw() {
   if (x > canvas.width) x = -50;
   requestAnimationFrame(draw);
 }
-function create() {
-  const canvas2 = document.getElementById("MarkFridlin");
-  const ctx2 = canvas2.getContext("2d");
-  ctx2.beginPath();
-  ctx2.moveTo(75, 250);
-  ctx2.lineTo(125, 250);
-  ctx2.lineTo(125, 150);
-  ctx2.lineTo(135, 150);
-  ctx2.lineTo(135, 350);
-  ctx2.lineTo(125, 350);
-  ctx2.lineTo(125, 500);
-  ctx2.lineTo(120, 500);
-  ctx2.lineTo(120, 350);
-  ctx2.lineTo(80, 350);
-  ctx2.lineTo(80, 500);
-  ctx2.lineTo(75, 500);
-  ctx2.fill();
-}
 onMounted(() => {
   draw();
-  create();
 });
 </script>
