41942ff77d5383f38be528cb971fcbd67bbc96c8
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-06-11 15:25:40 -0400

    Yeah eayh

--- QueaSY/src/views/StorePage.vue
+++ QueaSY/src/views/StorePage.vue
@@ -13,6 +13,7 @@
     </div>
     <div class="flex justify-around w-7/8">
       <h1>Tank Shop</h1>
+      <div><h1>Level 1</h1></div>
       <div><h1>Level 2 Tank</h1></div>
       <div><h1>Level 3 Tank</h1></div>
       <div><h1>Level 4 Tank</h1></div>
@@ -26,6 +27,7 @@
 </template>
 
 <script setup>
-function tankLevel() {}
-//sessionStorage.setItem(level, 1);
+function tankLevel1() {
+  sessionStorage.setItem(level, 1);
+}
 </script>
