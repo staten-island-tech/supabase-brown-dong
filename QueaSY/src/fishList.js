25345de4a843a828795eb20aba5f0d3491204ea9
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-06-11 20:42:51 -0400

    Just thinking

--- QueaSY/src/fishList.js
+++ QueaSY/src/fishList.js
@@ -6,7 +6,13 @@ export const fishList = [
     image: "WhiteAxolotl.png",
   },
   { name: "Magikarp", chance: 25, type: "swimmer", image: "Magikarp.png" },
-  { name: "Blue Pikmin", chance: 20, type: "walker", image: "BluePikmin.png" },
+  {
+    name: "Blue Pikmin",
+    chance: 20,
+    type: "walker",
+    image: "BluePikmin.png",
+    animation: ["BPIDle.png", "BPWalk1"],
+  },
   { name: "Poliwhirl", chance: 10, type: "swimmer", image: "Poliwhirl.png" },
   {
     name: "Spongebob Banana",
