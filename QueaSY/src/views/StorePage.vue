a8639b438920361d8ca6b76f2fe9278b4aad50ce
AlexanderDong09 <alexanderdong09@gmail.com>
2025-06-11 22:16:41 -0400

    Update StorePage.vue

--- QueaSY/src/views/StorePage.vue
+++ QueaSY/src/views/StorePage.vue
@@ -1,33 +1,7 @@
 <template>
-  <div
-    class="absolute w-[80rem] h-[50rem] flex flex-col top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 justify-center items-center"
-  >
-    <div class="flex justify-around w-7/8">
-      <h1>Game Shop</h1>
-      <div>
-        <h1>Coin Flip</h1>
-      </div>
-      <div>
-        <h1>Blackjack</h1>
-      </div>
-    </div>
-    <div class="flex justify-around w-7/8">
-      <h1>Tank Shop</h1>
-      <div><h1>Level 1</h1></div>
-      <div><h1>Level 2 Tank</h1></div>
-      <div><h1>Level 3 Tank</h1></div>
-      <div><h1>Level 4 Tank</h1></div>
-    </div>
-    <div class="flex justify-around w-7/8">
-      <h1>Egg Shop</h1>
-      <div><h1>Pokemon Egg</h1></div>
-      <div><h1>Legendary Egg</h1></div>
-    </div>
-  </div>
+  <div></div>
 </template>
 
-<script setup>
-function tankLevel1() {
-  sessionStorage.setItem(level, 1);
-}
-</script>
+<script setup></script>
+
+<style lang="scss" scoped></style>
